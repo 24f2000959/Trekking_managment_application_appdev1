8149f165a12fcafb1aa0e301258983245a636f1e
Robin <robinrao198@gmail.com>
2026-07-13 14:14:53 +0530

    Milestone- update Staff Dashboard and Trek Management

--- application/controllers.py
+++ application/controllers.py
@@ -694,9 +694,33 @@ def staff_dashboard():
         flash("first login")
         return redirect("login_page")
     
-    treks = Trek.query.all()
-    bookings = Booking.query.all()
-    return render_template("staff/staff_dashboard.html", this_user=this_user, treks=treks, bookings=bookings)
+   
+    return render_template("staff/staff_dashboard.html", this_user=this_user, )
+
+
+# XXXXXXXXXXXXXXXXXXXXXXxxxxx|my trek|XXXXXXXXXXXXXXXXXXXXXXX
+@app.route("/staff/my_treks_page")
+def my_treks_page():
+    if "user_id" not in session:
+        flash("login first !")
+        return redirect("/login_page")
+
+    if session.get("role") != "staff":
+        flash("Unauthorized Access")
+        return redirect("/login_page")
+    
+    user_id = session.get("user_id")  
+    this_user = User.query.filter_by(id=user_id).first()
+
+    if this_user is None :
+        session.clear()   
+        flash("first login")
+        return redirect("login_page")
+    
+    trek = Trek.query.filter_by(staff_id=this_user.id).all()
+    
+    return render_template("/staff/my_treks_page.html",this_user=this_user, trek=trek)
+
 
 
 
